47f25dc14bda24884b3b6cda4c1b042b4a3a5fb6
Dev Krishna <vaibhavchauhan658@gmail.com>
2024-07-13 13:17:02 +0530

    Add files via upload

---
+++ hospital/script.js
@@ -0,0 +1,9 @@
+script.js
+let menubar = document.querySelector('#menubar');
+let mynav = document.querySelector('.navbar');
+
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times')
+    mynav.classList.toggle('active')
+}
